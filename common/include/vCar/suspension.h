5602e9ee740814c6bb7b40a2d9fdd48e7e168d02
Kerry <kerryloux@gmail.com>
2015-07-15 09:10:04 -0400

    Working on issue #19

--- common/include/vCar/suspension.h
+++ common/include/vCar/suspension.h
@@ -123,36 +123,7 @@ public:
 	Damper frontThirdDamper;
 	Damper rearThirdDamper;
 
-	// Kinematic solvers
-	static bool SolveForPoint(const Vector &center1, const Vector &center2, const Vector &center3,
-		const Vector &originalCenter1, const Vector &originalCenter2, const Vector &originalCenter3,
-		const Vector &original, Vector &current);
-	static bool SolveForPoint(const Corner::Hardpoints &target, const Corner::Hardpoints& reference1,
-		const Corner::Hardpoints& reference2, const Corner::Hardpoints& reference3,
-		const Corner& originalCorner, Corner& currentCorner);
-	static bool SolveForXY(const Corner::Hardpoints &target, const Corner::Hardpoints& reference1,
-		const Corner::Hardpoints& reference2, const Corner& originalCorner, Corner& currentCorner);
-	static bool SolveForContactPatch(const Vector &wheelCenter, const Vector &wheelPlaneNormal,
-		const double &tireRadius, Vector &output);
-	static bool SolveInboardTBarPoints(const Vector &leftOutboard, const Vector &rightOutboard,
-		const Vector &centerPivot, const Vector &pivotAxisPoint,
-		const Vector &originalLeftOutboard, const Vector &originalRightOutboard,
-		const Vector &originalCenterPivot, const Vector &originalPivotAxisPoint,
-		const Vector &originalLeftInboard, const Vector &originalRightInboard,
-		Vector &leftInboard, Vector &rightInboard);
-
-	// Other functions
-	void MoveSteeringRack(const double &travel);
-
 	Suspension& operator=(const Suspension& suspension);
-
-private:
-	static Vector FindPerpendicularVector(const Vector &v);
-	static double OptimizeCircleParameter(const Vector &center, const Vector &a,
-		const Vector &b, const Vector &target);
-
-	static bool SolveForXY(const Vector &center1, const Vector &center2, const Vector &originalCenter1,
-		const Vector &originalCenter2, const Vector &original, Vector &current);
 };
 
 #endif// SUSPENSION_H_
--- common/include/vSolver/physics/kinematics.h
+++ common/include/vSolver/physics/kinematics.h
@@ -61,7 +61,6 @@ public:
 		bool operator != (const Kinematics::Inputs &target) const { return !(*this == target); };
 	};
 
-	// Private data accessors (SET)
 	inline void SetPitch(const double &pitch) { inputs.pitch = pitch; };
 	inline void SetRoll(const double &roll) { inputs.roll = roll; };
 	inline void SetHeave(const double &heave) { inputs.heave = heave; };
@@ -70,10 +69,8 @@ public:
 	inline void SetFirstEulerRotation(const Vector::Axis &first) { inputs.firstRotation = first; };
 	inline void SetInputs(Inputs _inputs) { inputs = _inputs; };
 
-	// Main work function for this class
 	void UpdateKinematics(const Car* _OriginalCar, Car* _WorkingCar, wxString Name);
 
-	// Private data accessors (GET)
 	inline KinematicOutputs GetOutputs(void) { return outputs; };
 	inline double GetPitch(void) { return inputs.pitch; };
 	inline double GetRoll(void) { return inputs.roll; };
@@ -96,6 +93,34 @@ private:
 		const Vector &rotations, const Vector::Axis &secondRotation);
 
 	void UpdateOutputs(void);
+
+	// Kinematic solvers
+	static bool SolveForPoint(const Vector &center1, const Vector &center2, const Vector &center3,
+		const Vector &originalCenter1, const Vector &originalCenter2, const Vector &originalCenter3,
+		const Vector &original, Vector &current);
+	static bool SolveForPoint(const Corner::Hardpoints &target, const Corner::Hardpoints& reference1,
+		const Corner::Hardpoints& reference2, const Corner::Hardpoints& reference3,
+		const Corner& originalCorner, Corner& currentCorner);
+	static bool SolveForXY(const Corner::Hardpoints &target, const Corner::Hardpoints& reference1,
+		const Corner::Hardpoints& reference2, const Corner& originalCorner, Corner& currentCorner);
+	static bool SolveForContactPatch(const Vector &wheelCenter, const Vector &wheelPlaneNormal,
+		const double &tireRadius, Vector &output);
+	static bool SolveInboardTBarPoints(const Vector &leftOutboard, const Vector &rightOutboard,
+		const Vector &centerPivot, const Vector &pivotAxisPoint,
+		const Vector &originalLeftOutboard, const Vector &originalRightOutboard,
+		const Vector &originalCenterPivot, const Vector &originalPivotAxisPoint,
+		const Vector &originalLeftInboard, const Vector &originalRightInboard,
+		Vector &leftInboard, Vector &rightInboard);
+
+	// Other functions
+	void MoveSteeringRack(const double &travel) const;
+
+	static Vector FindPerpendicularVector(const Vector &v);
+	static double OptimizeCircleParameter(const Vector &center, const Vector &a,
+		const Vector &b, const Vector &target);
+
+	static bool SolveForXY(const Vector &center1, const Vector &center2, const Vector &originalCenter1,
+		const Vector &originalCenter2, const Vector &original, Vector &current);
 };
 
 #endif// _KINEMATICS_H_
