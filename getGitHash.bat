c646a0c83d101e32f5e108c231c4ede26c306678
Kerry <kerryloux@gmail.com>
2016-06-09 13:02:32 -0400

    Changed TeX version to pull from git tags

--- getGitHash.bat
+++ getGitHash.bat
@@ -5,14 +5,20 @@ REM This is the file to which output will be written
 SET outputPath="%1CarDesigner/src/application/"
 SET outputFile=vvaseConstants.cpp
 
+SET texPath="%1manual/"
+SET texFile=gitHash.sty
+
 FOR /f %%i IN ('git describe --tags --abbrev^=0') DO SET gitTag=%%i
 FOR /f %%i IN ('git rev-parse --short HEAD') DO SET gitHash=%%i
 
 REM ECHO outputPath = %outputPath%
 REM ECHO outputFile = %outputFile%
+REM ECHO texPath = %texPath%
+REM ECHO texFile = %texFile%
 ECHO version = %gitTag%
 ECHO gitHash = %gitHash%
 
+REM Build source file
 @ECHO // File:  %outputFile%> %outputPath%%outputFile%
 @ECHO // Auth:  getGitHash.bat>> %outputPath%%outputFile%
 @ECHO // Desc:  Automatically generated file containing latest git hash and version info.>> %outputPath%%outputFile%
@@ -22,3 +28,11 @@ ECHO gitHash = %gitHash%
 @ECHO.>> %outputPath%%outputFile%
 @ECHO const wxString carDesignerVersion = _T("%gitTag%");>> %outputPath%%outputFile%
 @ECHO const wxString carDesignerGitHash = _T("%gitHash%");>> %outputPath%%outputFile%
+
+REM Build TeX file
+@ECHO %% %texFile%> %texPath%%texFile%
+@ECHO \ProvidesPackage{gitHash}[6/9/2016 for VVASE]>> %texPath%%texFile%
+@ECHO.>> %texPath%%texFile%
+@ECHO % Common commands>> %texPath%%texFile%
+@ECHO \newcommand{\version}{%gitTag%}>> %texPath%%texFile%
+@ECHO \newcommand{\gitHash}{%gitHash%}>> %texPath%%texFile%
--- getGitHash.sh
+++ getGitHash.sh
@@ -5,14 +5,20 @@
 outputPath="$1CarDesigner/src/application/"
 outputFile=vvaseConstants.cpp
 
+texPath="$1manual/"
+texFile=gitHash.sty
+
 gitTag=$(git describe --tags --abbrev=0)
 gitHash=$(git rev-parse --short HEAD)
 
 #echo outputPath = $outputPath
 #echo outputFile = $outputFile
+#echo texPath = $texPath
+#echo texFile = $texFile
 echo version = $gitTag
 echo gitHash = $gitHash
 
+# Build source file
 echo // File:  $outputFile> $outputPath$outputFile
 echo // Auth:  getGitHash.sh>> $outputPath$outputFile
 echo // Desc:  Automatically generated file containing latest git hash and version info.>> $outputPath$outputFile
@@ -22,3 +28,12 @@ echo \#include \"application/vvaseConstants.h\">> $outputPath$outputFile
 echo>> $outputPath$outputFile
 echo const wxString carDesignerVersion = _T\(\"$gitTag\"\)\;>> $outputPath$outputFile
 echo const wxString carDesignerGitHash = _T\(\"$gitHash\"\)\;>> $outputPath$outputFile
+
+
+# Build TeX style file
+echo %% $texFile> $texPath$texFile
+echo \\ProvidesPackage{gitHash}[6/9/2016 for VVASE]>> $texPath$texFile
+echo>> $texPath$texFile
+echo % Common commands>> $texPath$texFile
+echo \\newcommand{\\version}{$gitTag}>> $texPath$texFile
+echo \\newcommand{\\gitHash}{$gitHash}>> $texPath$texFile
